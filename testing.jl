48f9c1e433f516fa365c477a17251a7fa6c48151
stepinski <stepinski@gmail.com>
2021-02-26 14:45:40 +0100

    testing structures jl

--- testing.jl
+++ testing.jl
@@ -1,3 +1,10 @@
 println("tell me sth")
 test_me=100
 typeof(test_me)
+tsstr=""""abba" test chars in string"""
+println(tsstr)
+typeof('a')
+
+myphonebook = Dict("Jenny" => "867-5309", "Ghostbusters" => "555-2368")
+myphonebook["Jenny"]
+rand(4,3)
