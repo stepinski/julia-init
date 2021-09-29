83abd6fdf32ced2d427a58ccac152996121eb3a5
stepinski <stepinski@gmail.com>
2021-09-29 10:54:43 +0200

    pangram test

--- pangram/pangram.jl
+++ pangram/pangram.jl
@@ -5,6 +5,15 @@ Return `true` if `input` contains every alphabetic character (case insensitive).
 
 """
 function ispangram(input)
+    alphabet="abcdefghijklmnopqrstuvwxyz"
 
+    for c in lowercase(input)
+        if c in alphabet
+            alphabet=replace(alphabet,c=>"")
+        end 
+    end
+
+    # return alphabet
+    return (alphabet == "")
 end
 
