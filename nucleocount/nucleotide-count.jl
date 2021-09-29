64bb522a0c9563a82727441ec3ba37fc67ab2687
stepinski <stepinski@gmail.com>
2021-09-29 10:13:08 +0200

    nucleo passing

--- nucleocount/nucleotide-count.jl
+++ nucleocount/nucleotide-count.jl
@@ -7,5 +7,13 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-
+    res = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    for c in strand
+        if c in ['A','C','G','T']
+            res[c] = get(res,c,0)+1
+        else 
+            throw(DomainError(c, "expects A,C,G, or T"))
+        end
+    end
+    return res
 end
