6ce5d2873f485189faceea3235c0eaf85bde8bab
stepinski <stepinski@gmail.com>
2021-10-04 17:19:14 +0200

    passing shake

--- secret-handshake/secret-handshake.jl
+++ secret-handshake/secret-handshake.jl
@@ -1,3 +1,22 @@
 function secret_handshake(code)
+    bits=bitstring(code)
+    uncode=reverse(bits)
+    ret = String[]
+    if uncode[1]=='1'
+        push!(ret,"wink")
+    end
+    if uncode[2]=='1'
+        push!(ret,"double blink")
+    end
+    if(uncode[3]=='1')
+        push!(ret,"close your eyes")
+    end
+    if(uncode[4]=='1')
+        push!(ret,"jump")
+    end
 
+    if(uncode[5]=='1')
+        ret=reverse(ret)
+    end
+    return ret
 end
