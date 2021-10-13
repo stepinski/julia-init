de73432bdf102c5c5e0de72187a7b3a4b55650ac
stepinski <stepinski@gmail.com>
2021-10-13 15:05:18 +0200

    rna passing

--- rna-transcription/rna-transcription.jl
+++ rna-transcription/rna-transcription.jl
@@ -1,4 +1,22 @@
 function to_rna(dna)
+    trans = ""
+    for n in dna
+        # print(n)
+        if n == ""
+            trans = trans
+        elseif n == 'C'
+            trans *= 'G'
+        elseif n == 'G'
+            trans *= 'C'
+        elseif n == 'T'
+            trans *= 'A'
+        elseif n == 'A'
+            trans *= 'U'
+        else
+            throw(ErrorException("bad input"))
+        end
+    end
 
+    return trans
 end
 
