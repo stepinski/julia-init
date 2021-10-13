94e815f11e3c194a3548d3f8927ba5d812c1abc4
stepinski <stepinski@gmail.com>
2021-10-13 17:05:11 +0200

    rle almost

--- run-length-encoding/run-length-encoding.jl
+++ run-length-encoding/run-length-encoding.jl
@@ -1,7 +1,47 @@
+function codepart(cnt,prevc)
+    codepart=cnt>1 ? string(cnt) : ""
+    return codepart * prevc
+end
+
 function encode(s)
+    prevc = s[1]
+    coded = ""
+    cnt=0
+
+    for c in s
+        if c == prevc
+            cnt = cnt + 1 
+        else
+            codepart=cnt>1 ? string(cnt) : ""
+            coded = coded * codepart(cnt,prevc)
+            # prevc = ""
+            cnt = 0
+        end 
+        prevc = c   
+    end
+    
+    return coded
 end
 
 
 
+
 function decode(s)
+    concat = ""
+    decoded = ""
+
+    for c in s
+        isnumb = isnumeric(c)
+        
+        if isnumb
+            # collect new chars as part of number
+            concat = concat * c
+        else
+            cnt = parse(Int64, concat)
+            decoded = decoded + repeat(c, cnt)
+            concat = ""
+
+        end    
+    end
+    return coded
 end
