4f8805a414b7474cde5c6ef647b23314ee182327
stepinski <stepinski@gmail.com>
2021-11-16 11:43:20 +0100

    wc improved

--- word-count/word-count.jl
+++ word-count/word-count.jl
@@ -1,29 +1,46 @@
 function wordcount(sentence)
-    prev = nothing
+    prev = ""
     prevclass = 0
-    res = Dict{String, Int32}()
+    res = Dict{String,Int32}()
 
     for c in sentence
-        if isnumeric(c)
-            print(c* " is numeric")
-            if prevclass == 1
+        
+        if isspace(c)
+        # separate word
+            if length(prev) > 0 
+                tmp = get!(res, prev, 0)
+                push!(res, prev => tmp + 1)
+                prev=""
+            end
+    
+        # elseif isnumeric(c)
+        #     print(c * " is numeric")
+        #     if prevclass == 0 
+        #         # we shall add previous word
+                
+        #     elseif prevclass == 2
+        #         # push!(res, "sdf" => [1, 2])
+        #         res[prev] = res[prev] + 1 
+        #         prev = c
+        #         prevclass = 1
+        #     end
+        
+        elseif isletter(c) || isnumeric(c)
+            print(c * " is ascii")
+            if prevclass == 0
                 prev = prev * c
-            elseif prevclass == 2
-                # push!(res, "sdf" => [1, 2])
-                res[prev] = res[prev] + 1 
-                prev = c
-                prevclass = 1
-            end 
-        elseif isascii(c)
-            print(c* " is ascii")
-            if prevclass == 1
-                res[prev] = res[prev] + 1 
-                prev = c
-                prevclass = 2
-           elseif prevclass == 2
-                prev = prev * c
+           elseif prevclass == 1
+                throw("not implemented")
            end 
+        
+        
         end
+    end
+    # adding remaining word to dictionary
+    if length(prev) > 0 
+        tmp = get!(res, prev, 0)
+        push!(res, prev => tmp + 1)
+        # res[prev] = res[prev] + 1 
     end
 
     return res
