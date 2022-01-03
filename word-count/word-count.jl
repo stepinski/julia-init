c2115e3f6e375fec55fbe45b4f53fdf2397eda01
stepinski <stepinski@gmail.com>
2022-01-03 19:31:24 +0100

    fixing split and concatenating

--- word-count/word-count.jl
+++ word-count/word-count.jl
@@ -1,14 +1,32 @@
 function wordcount(sentence)
 
-    tmp=lowercase(sentence)
-    tmp=replace(tmp, "' "  =>"")
-    tmp=replace(tmp," '" =>"")
-    tmp=replace(tmp,"'" =>"T")
-    words=split(tmp)
-    for w in words
-        tst=replace(w, !isletter =>"")
-        println(tst)
+    function isnon(c)
+        return !isletter(c) & !isdigit(c)
     end
+    tmp = lowercase(sentence)
+    #tmp = replace(tmp, "' " => "")
+    #tmp = replace(tmp, " '" => "")
+    tmp = replace(tmp, "'" => "T")
+    words = split(tmp,[',',' ','\t','\n'])
+    res = Dict{String,Int32}()
+    for w in words
+        tst = replace(w, isnon => "")
+        if last(tst,1)=="T" 
+            tst=chop(tst)
+        end
+        if first(tst,1) =="T"
+            tst=reverse(chop(reverse(tst)))
+        end
+
+        tst = replace(tst, "T" => "'")
+
+        if tst != "T" && length(tst)>0
+            #println("out $tst")
+            tmp = get!(res, tst, 0)
+            push!(res, tst => tmp + 1)
+        end
+    end
+    return res
 end
 function wordcounts(sentence)
     prev = ""
@@ -17,40 +35,40 @@ function wordcounts(sentence)
 
 
     for c in lowercase(sentence)
-        
-        if isspace(c) || c==','
-        # separate word
-            if length(prev) > 0 
+
+        if isspace(c) || c == ','
+            # separate word
+            if length(prev) > 0
                 tmp = get!(res, prev, 0)
                 push!(res, prev => tmp + 1)
-                prev=""
+                prev = ""
             end
-    
-        # elseif isnumeric(c)
-        #     print(c * " is numeric")
-        #     if prevclass == 0 
-        #         # we shall add previous word
-                
-        #     elseif prevclass == 2
-        #         # push!(res, "sdf" => [1, 2])
-        #         res[prev] = res[prev] + 1 
-        #         prev = c
-        #         prevclass = 1
-        #     end
-        
-        elseif isletter(c) || isnumeric(c) || (c==''' && length(prev)>0)
+
+            # elseif isnumeric(c)
+            #     print(c * " is numeric")
+            #     if prevclass == 0 
+            #         # we shall add previous word
+
+            #     elseif prevclass == 2
+            #         # push!(res, "sdf" => [1, 2])
+            #         res[prev] = res[prev] + 1 
+            #         prev = c
+            #         prevclass = 1
+            #     end
+
+        elseif isletter(c) || isnumeric(c) || (c == ''' && length(prev) > 0)
             print(c * " is ascii")
             if prevclass == 0
                 prev = prev * c
-           elseif prevclass == 1
+            elseif prevclass == 1
                 throw("not implemented")
-           end 
-        
-        
+            end
+
+
         end
     end
     # adding remaining word to dictionary
-    if length(prev) > 0 
+    if length(prev) > 0
         tmp = get!(res, prev, 0)
         push!(res, prev => tmp + 1)
         # res[prev] = res[prev] + 1 
