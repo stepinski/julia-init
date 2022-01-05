e04e9ff054f137d568a4f6192a1792f299a00f36
stepinski <stepinski@gmail.com>
2022-01-05 19:10:19 +0100

    julia etl

--- etl/etl.jl
+++ etl/etl.jl
@@ -1,4 +1,10 @@
 function transform(input::AbstractDict)
-
+    out= Dict{Char, Int64}()
+    for (k, v) in input
+        for i in v
+            push!(out,lowercase(i)=>k)            
+        end
+    end
+    return out
 end
 
