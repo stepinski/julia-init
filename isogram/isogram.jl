8501ac22b60d56c337ad0effb4f92a2982e2ecd1
stepinski <stepinski@gmail.com>
2022-01-10 18:41:43 +0100

    fixed isogram

--- isogram/isogram.jl
+++ isogram/isogram.jl
@@ -1,12 +1,13 @@
 function isisogram(s)
     tst = replace(lowercase(s), !isletter => "")
-    prev=""
-    isok=true
+    prev = ""
+    isok = true
     for l in sort(collect(tst))
-        if prev==l
-            isok=false
+        if prev == l
+            isok = false
             break
         end
+        prev = l
     end
     return isok
 end
