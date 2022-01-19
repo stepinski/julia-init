917ac2fefc82c9e798c58dab021ab1614c4f3fb9
stepinski <stepinski@gmail.com>
2022-01-19 21:50:56 +0100

    binary search changes

--- binary-search/binary-search.jl
+++ binary-search/binary-search.jl
@@ -1,25 +1,33 @@
 
 
-function binarysearch(arr, item, cnt = 1)
-    size = length(arr)
-    idx = floor(Int, size / 2 + 0.5)
-    retval = 0
-    if size > 2
-        if arr[idx] > item
-            binarysearch(arr[1:idx], item, cnt + 1)
+function binarysearch(arr,low,high,item,cnt)
+    # idx = floor(Int, size / 2 + 0.5)
+    idx=low+floor(Int, (high-low) / 2 )
+    if high>=low
+        # idx = floor(Int, size / 2 + 0.5)
+        if arr[idx]==item
+            return idx:cnt
+        elseif arr[idx] > item
+            binarysearch(arr,low,high-1, item, cnt + 1)
         elseif arr[idx] < item
-            binarysearch(arr[idx:size], item, cnt + 1)
-        else
-            retval = idx
-            # return idx:cnt
+            binarysearch(arr,idx+1,high, item, cnt + 1)
         end
+    else 
+        return idx:cnt
+    end
     # TODO add different conditions for corner scenarios    
         # elseif size==2
     #     if arr[1]==
-    end
+    #end
     # if idx == 1
     #     return 0:cnt
     # end
     # end
-    return retval:cnt
+    #return retval:cnt
+end
+
+function binarysearch(arr, item)
+    size=length(arr)
+    ret=binarysearch(arr,1,size,item,0)
+    return ret
 end
