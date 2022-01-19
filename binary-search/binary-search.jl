8e360e4839bce92422c5b99ecbd1bd2c28b85533
stepinski <stepinski@gmail.com>
2022-01-19 22:16:22 +0100

    b search upgrade

--- binary-search/binary-search.jl
+++ binary-search/binary-search.jl
@@ -1,22 +1,37 @@
 
 
-function binarysearch(arr,low,high,item,cnt)
+function binarysearch(arr, low, high, item, mode, result)
     # idx = floor(Int, size / 2 + 0.5)
-    idx=low+floor(Int, (high-low) / 2 )
-    if high>=low
+    idx = low + floor(Int, (high - low) / 2)
+    # print(low)
+    # print(high)
+    if high >= low
         # idx = floor(Int, size / 2 + 0.5)
-        if arr[idx]==item
-            return idx:cnt
+        if arr[idx] == item
+            if mode == 0
+                result[1] = idx
+            elseif mode == 1
+                result[2] = idx
+            end
+
         elseif arr[idx] > item
-            binarysearch(arr,low,high-1, item, cnt + 1)
+            binarysearch(arr, low, high - 1, item, 1, result)
         elseif arr[idx] < item
-            binarysearch(arr,idx+1,high, item, cnt + 1)
+            binarysearch(arr, idx + 1, high, item, 0, result)
         end
-    else 
-        return idx:cnt
+    else
+        if result == [0,0]
+            result = [low, low - 1]
+        
+        elseif result[2]==0
+            result[2]=result[1]
+        end  
+        return result[1]:result[2]
     end
+
+    
     # TODO add different conditions for corner scenarios    
-        # elseif size==2
+    # elseif size==2
     #     if arr[1]==
     #end
     # if idx == 1
@@ -27,7 +42,7 @@ function binarysearch(arr,low,high,item,cnt)
 end
 
 function binarysearch(arr, item)
-    size=length(arr)
-    ret=binarysearch(arr,1,size,item,0)
+    size = length(arr)
+    ret = binarysearch(arr, 1, size, item, -1, [0, 0])
     return ret
 end
