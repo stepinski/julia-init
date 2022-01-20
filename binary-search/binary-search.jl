ed0fb80f20d970ead044253c3c9bcc1d808411ce
stepinski <stepinski@gmail.com>
2022-01-20 12:28:48 +0100

    binary search ok

--- binary-search/binary-search.jl
+++ binary-search/binary-search.jl
@@ -1,30 +1,51 @@
 
 
-function binarysearch(arr, low, high, item, mode, result)
+function binarysearch(arr, low, high, item, result)
     # idx = floor(Int, size / 2 + 0.5)
     idx = low + floor(Int, (high - low) / 2)
-    # print(low)
-    # print(high)
+    # print("primoif")
+    #         print(low)
+    #         print(" ")
+    #         print(result[1])
+    #         print(" ")
+    #         print(high)    
+    #         print(" ")
+    #         print(result[2])
     if high >= low
         # idx = floor(Int, size / 2 + 0.5)
         if arr[idx] == item
-            if mode == 0
+            # print("index="*string(idx))
+            # print("mode="*string(mode))
+            
+            if result==[0,0]
+                result[1]= idx
+                result[2] = idx
+            elseif  idx<result[1]                 
                 result[1] = idx
-            elseif mode == 1
+            elseif idx>result[2]
                 result[2] = idx
             end
-
+            binarysearch(arr, low, idx - 1, item, result)
+            binarysearch(arr, idx + 1, high, item, result)
+            # print("test")
+            # print(low)
+            # print(" ")
+            # print(result[1])
+            # print(" ")
+            # print(high)    
+            # print(" ")
+            # print(result[2])
         elseif arr[idx] > item
-            binarysearch(arr, low, high - 1, item, 1, result)
+            binarysearch(arr, low, idx - 1, item,  result)
         elseif arr[idx] < item
-            binarysearch(arr, idx + 1, high, item, 0, result)
+            binarysearch(arr, idx + 1, high, item, result)
         end
     else
         if result == [0,0]
             result = [low, low - 1]
         
-        elseif result[2]==0
-            result[2]=result[1]
+        # elseif result[2]==0
+        #     result[2]=result[1]
         end  
         return result[1]:result[2]
     end
@@ -43,6 +64,6 @@ end
 
 function binarysearch(arr, item)
     size = length(arr)
-    ret = binarysearch(arr, 1, size, item, -1, [0, 0])
+    ret = binarysearch(arr, 1, size, item, [0, 0])
     return ret
 end
