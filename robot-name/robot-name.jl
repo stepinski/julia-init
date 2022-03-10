d95049678463b0aaedd75494c5054edff903a423
stepinski <stepinski@gmail.com>
2022-03-10 22:59:27 +0100

    random strings

--- robot-name/robot-name.jl
+++ robot-name/robot-name.jl
@@ -1,12 +1,15 @@
+using Random
 mutable struct Robot
-    obj::Robot
-    Robot() = (x = new(); x.obj = x;)
+    pname::String
+    # obj::Robot
+    Robot() = (x = new();x.pname="")
 end
 
 function reset!(instance::Robot)
 
 end
 
-function name(instance::Robot)
-    return ""
+function name(instance::Robot) 
+    instance.pname = length(instance.pname)>0 ? randstring('A':'Z', 2) * randstring('0':'9', 3) : instance.pname
+    return instance.pname
 end
