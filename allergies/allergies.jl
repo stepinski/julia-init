07aa7c2e4a7b4da683c281d09020af4d95a745ee
stepinski <stepinski@gmail.com>
2022-04-11 22:14:29 +0200

    1st ver

--- allergies/allergies.jl
+++ allergies/allergies.jl
@@ -5,4 +5,5 @@ end
 function allergy_list(score)
     vals=Dict( 1 => "eggs", 2=>"peanuts"  , 4=>"shellfish"  , 8=>"strawberries"  , 16=>"tomatoes", 32=>"chocolate" , 64=>"pollen" , 128=>"cats")
     return Set([vals[score]])
+    
 end
