e25795f2c1b7c7b9fa6d9a568f69001d6381c257
stepinski <stepinski@gmail.com>
2022-04-14 23:10:04 +0200

    1st version anagram

--- anagram/anagram.jl
+++ anagram/anagram.jl
@@ -1,3 +1,17 @@
 function detect_anagrams(subject, candidates)
+    retset = []
+    for candidate in candidates
+        i=0
+        for c in candidate
+           if ! (c in subject)
+                break
+           end
+           i+=1
+        end
 
+        if i == length(candidate) == length(subject)
+            push!(retset,candidate)
+        end    
+    end
+    return retset
 end
